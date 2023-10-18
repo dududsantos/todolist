e2db88a685bc8215f710b44d32a667001f40bef4
eduardosantos <eduardo.victor.017@ufrn.edu.br>
2023-10-18 15:07:59 -0300

    inciando a aula 4, com algumas verificações

--- todolist/src/main/java/br/com/dududsantos/todolist/filter/FilterTaskAuth.java
+++ todolist/src/main/java/br/com/dududsantos/todolist/filter/FilterTaskAuth.java
@@ -27,33 +27,41 @@ public class FilterTaskAuth extends OncePerRequestFilter {
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
 
-    var authorization = request.getHeader("Authorization");
+    var serveletPath = request.getServletPath();
 
-    var authEncoded = authorization.substring("Basic".length()).trim();
+    if (serveletPath.equals("/tasks/")) {
 
-    byte[] authDecode = Base64.getDecoder().decode(authEncoded);
-    var authString = new String(authDecode);
+      var authorization = request.getHeader("Authorization");
 
-    String[] credentials = authString.split(":");
-    String userName = credentials[0];
-    String password = credentials[1];
+      var authEncoded = authorization.substring("Basic".length()).trim();
 
-    var user = this.userRepository.findByUserName(userName);
-    if (user == null) {
-      response.sendError(401);
+      byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+      var authString = new String(authDecode);
 
-    } else {
+      String[] credentials = authString.split(":");
+      String userName = credentials[0];
+      String password = credentials[1];
 
-      var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-
-      if (passwordVerify.verified) {
-        filterChain.doFilter(request, response);
-      } else {
+      var user = this.userRepository.findByUserName(userName);
+      if (user == null) {
         response.sendError(401);
+
+      } else {
+
+        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+        if (passwordVerify.verified) {
+          request.setAttribute("idUser", user.getId());
+          filterChain.doFilter(request, response);
+        } else {
+          response.sendError(401);
+        }
+
       }
 
+    } else {
+      filterChain.doFilter(request, response);
     }
-
   }
 
 }
